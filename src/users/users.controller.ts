1ca6ca5b21e0ae93c25bf8af3fd396d39213f485
rohitsinghmehra-sdd <rohitsinghmehra@smartdatainc.net>
2023-12-18 17:07:48 +0530

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
